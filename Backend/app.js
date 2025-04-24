2bc898687b5bbce146a099400d94121ba6731ecd
Atharva A Mahajan <mahajanatharva98@outlook.com>
2025-04-25 00:23:20 +0530

    tell

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // Middleware Setup
 const corsOptions = {
-  origin: "*", // Allow your frontend domain and others
+  origin: [
+    "http://localhost:3000",
+    "https://lda-govschemes.vercel.app/"
+  ], // Allow your frontend domain and others
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Enable cookies in requests and responses
